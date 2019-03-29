8e38d53b5f07c3047ff0ab22b7c2b3f99589ce80
Jill Heske <jill.heske@udacity.com>
2019-03-29 10:22:15 -0400

    Step.04-Solution-Add-VideosDatabase

--- app/src/main/java/com/example/android/devbyteviewer/database/Room.kt
+++ app/src/main/java/com/example/android/devbyteviewer/database/Room.kt
@@ -17,11 +17,9 @@
 
 package com.example.android.devbyteviewer.database
 
+import android.content.Context
 import androidx.lifecycle.LiveData
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
-import androidx.room.Query
+import androidx.room.*
 
 @Dao
 interface VideoDao {
@@ -32,15 +30,20 @@ interface VideoDao {
     fun insertAll(vararg videos: DatabaseVideo)
 }
 
-// TODO (01) Create an abstract VideosDatabase class that extends RoomDatabase.
+@Database(entities = [DatabaseVideo::class], version = 1)
+abstract class VideosDatabase : RoomDatabase() {
+    abstract val videoDao: VideoDao
+}
 
-// TODO (02) Annotate VideosDatabase with @Database,including entities and version.
+private lateinit var INSTANCE: VideosDatabase
 
-// TODO (03) Inside VideosDatabase, create abstract val videoDao.
-
-// TODO (04) Create an INSTANCE variable to store the VideosDatabase singleton.
-
-// TODO (05) Define a function getDatabase() that returns the VideosDatabase INSTANCE.
-
-// TODO (06) Inside getDatabase(), before returning INSTANCE, use a synchronized{} block to
-// check whether INSTANCE is initialized, and, if it isn’t, use DatabaseBuilder to create it.
+fun getDatabase(context: Context): VideosDatabase {
+    synchronized(VideosDatabase::class.java) {
+        if (!::INSTANCE.isInitialized) {
+            INSTANCE = Room.databaseBuilder(context.applicationContext,
+                    VideosDatabase::class.java,
+                    "videos").build()
+        }
+    }
+    return INSTANCE
+}
