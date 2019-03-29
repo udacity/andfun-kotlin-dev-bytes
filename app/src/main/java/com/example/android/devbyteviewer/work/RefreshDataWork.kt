087d5c092765aa138b2d1ab2dfcf9269ec07d1c4
Jill Heske <jill.heske@udacity.com>
2019-03-29 16:05:59 -0400

    Step.07-Exercise-Create-a-Worker

--- app/src/main/java/com/example/android/devbyteviewer/work/RefreshDataWork.kt
+++ app/src/main/java/com/example/android/devbyteviewer/work/RefreshDataWork.kt
@@ -16,3 +16,12 @@
  */
 
 package com.example.android.devbyteviewer.work
+
+// TODO (01) Create the RefreshDataWorker class, extend it from CoroutineWorker, and
+// pass in a Context and WorkerParams.
+
+// TODO (02) Override the required doWork() method, and create variables for the
+// database and the repository.
+
+// TODO (03) Inside doWork(), in a try-catch block, refresh the videos, and
+// use Payload() to return SUCCESS or RETRY result.
