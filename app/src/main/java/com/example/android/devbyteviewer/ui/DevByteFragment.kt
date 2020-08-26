81d8117964fc8931321bc9c68967ace87b5ad545
SudKul <sudhanshu.kulshrestha@gmail.com>
2020-08-26 16:54:02 +0530

    Update comments related to the Fragment lifecycle method onViewCreated()

--- app/src/main/java/com/example/android/devbyteviewer/ui/DevByteFragment.kt
+++ app/src/main/java/com/example/android/devbyteviewer/ui/DevByteFragment.kt
@@ -44,12 +44,12 @@ class DevByteFragment : Fragment() {
 
     /**
      * One way to delay creation of the viewModel until an appropriate lifecycle method is to use
-     * lazy. This requires that viewModel not be referenced before onActivityCreated, which we
+     * lazy. This requires that viewModel not be referenced before onViewCreated(), which we
      * do in this Fragment.
      */
     private val viewModel: DevByteViewModel by lazy {
         val activity = requireNotNull(this.activity) {
-            "You can only access the viewModel after onActivityCreated()"
+            "You can only access the viewModel after onViewCreated()"
         }
         //The ViewModelProviders (plural) is deprecated.  
         //ViewModelProviders.of(this, DevByteViewModel.Factory(activity.application)).get(DevByteViewModel::class.java)
