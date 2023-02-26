23616d4062cec84d758f46de864c77e1487d3249
Tarek-Bohdima <tarek.buhdeima@gmail.com>
2023-02-27 00:39:24 +0200

    update gradle version update dependencies update theme in styles.xml update AndroidManifest.xml update build.gradle update viewbinding update Glide module class to use GlideApp instead of Glide in AppNameGlideModule.kt and BindingAdapters.kt update kotlin version

--- app/src/main/java/com/example/android/devbyteviewer/util/BindingAdapters.kt
+++ app/src/main/java/com/example/android/devbyteviewer/util/BindingAdapters.kt
@@ -20,7 +20,6 @@ package com.example.android.devbyteviewer.util
 import android.view.View
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
-import com.bumptech.glide.Glide
 
 /**
  * Binding adapter used to hide the spinner once data is available
@@ -35,5 +34,5 @@ fun goneIfNotNull(view: View, it: Any?) {
  */
 @BindingAdapter("imageUrl")
 fun setImageUrl(imageView: ImageView, url: String) {
-    Glide.with(imageView.context).load(url).into(imageView)
+    GlideApp.with(imageView.context).load(url).into(imageView)
 }
